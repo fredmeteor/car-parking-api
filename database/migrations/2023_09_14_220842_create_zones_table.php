b8da4418ac9506fd2f66c4277ff67e8a2601d888
fredmeteor <fredpeter09899@gmail.com>
2023-09-15 01:40:18 +0300

    Modified migration files

--- database/migrations/2023_09_14_220842_create_zones_table.php
+++ database/migrations/2023_09_14_220842_create_zones_table.php
@@ -13,8 +13,15 @@ return new class extends Migration
     {
         Schema::create('zones', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->integer('price_per_hour');
             $table->timestamps();
+
         });
+         Zone::create(['name' => 'Green Zone', 'price_per_hour' => 100]);
+        Zone::create(['name' => 'Yellow Zone', 'price_per_hour' => 200]);
+        Zone::create(['name' => 'Red Zone', 'price_per_hour' => 300]);
     }
 
     /**

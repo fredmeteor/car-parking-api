5c81137c942b6af746a54234bb35996747216f18
fredmeteor <fredpeter09899@gmail.com>
2023-09-15 02:45:39 +0300

    Modified the Auth controllers

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
 
 class RegisterController extends Controller
 {
-    //
+    public function __invoke(Request $request)
+    {
+        $request->validate([
+            'name'     => ['required', 'string', 'max:255'],
+            'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ], Response::HTTP_CREATED);
+    }
 }

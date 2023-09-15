d88ecc529f8b5b571bff31d7f504c5e2ad15542d
fredmeteor <fredpeter09899@gmail.com>
2023-09-15 04:14:47 +0300

    Modified vehicle controller

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class VehicleController extends Controller
 {
-    //
+      public function index()
+    {
+        return VehicleResource::collection(Vehicle::all());
+    }
+
+    public function store(StoreVehicleRequest $request)
+    {
+        $vehicle = Vehicle::create($request->validated());
+
+        return VehicleResource::make($vehicle);
+    }
+
+    public function show(Vehicle $vehicle)
+    {
+        return VehicleResource::make($vehicle);
+    }
+
+    public function update(StoreVehicleRequest $request, Vehicle $vehicle)
+    {
+        $vehicle->update($request->validated());
+
+        return response()->json(VehicleResource::make($vehicle), Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy(Vehicle $vehicle)
+    {
+        if ($vehicle->hasActiveParkings()) {
+            return response()->json([
+                'errors' => ['general' => ['Can\'t remove vehicle with active parkings. Stop active parking.']],
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $vehicle->delete();
+
+        return response()->noContent();
+    }
 }

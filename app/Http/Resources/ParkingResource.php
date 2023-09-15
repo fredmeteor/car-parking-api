b75720cf191096cc94947a70f254ab73bb4c0e36
fredmeteor <fredpeter09899@gmail.com>
2023-09-15 03:18:36 +0300

    Modified Parking resource

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -14,6 +14,25 @@ class ParkingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $totalPrice = $this->total_price ?? ParkingPriceService::calculatePrice(
+            $this->zone_id,
+            $this->start_time,
+            $this->stop_time
+        );
+
+        return [
+            'id'          => $this->id,
+            'zone'        => [
+                'name'           => $this->zone->name,
+                'price_per_hour' => $this->zone->price_per_hour,
+            ],
+            'vehicle'     => [
+                'plate_number' => $this->vehicle->plate_number,
+                'description'  => $this->vehicle->description,
+            ],
+            'start_time'  => $this->start_time->toDateTimeString(),
+            'stop_time'   => $this->stop_time?->toDateTimeString(),
+            'total_price' => $totalPrice,
+        ];
     }
 }
